303c925e922d51833ea58948d7c918c4dc0ea2c5
Sri Harsha Kappala <sriharsha.k@caratlane.com>
2018-03-31 13:30:10 +0530

    Add service method to check valid package.json file

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -2,6 +2,7 @@ class GithubService
 
   DOMAIN = 'https://api.github.com'
   SEARCH_URL = '/search/repositories'
+  RAW_FILE_DOMAIN = 'https://raw.githubusercontent.com'
 
   def self.search_repositories repo
     return JSON.parse($redis.get(repo)) if $redis.get(repo).present?
@@ -15,6 +16,21 @@ class GithubService
     results = response.read_body
     $redis.set(repo, results)
     return JSON.parse(results)
+  end
+
+  def self.check_for_package_json repo
+    url = URI(RAW_FILE_DOMAIN + '/' + repo + '/master/package.json')
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(url)
+    response = http.request(request)
+    binding.pry
+    if response.code == '200'
+      return "Your job has added to queue. This repo will be imported shortly!"
+    else
+      return "This repo doesn't have any package.json file"
+    end
   end
 
 end
