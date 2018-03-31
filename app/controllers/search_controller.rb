dd0618eba63d8eca8a7a274cafc4b5fdd767092f
Sri Harsha Kappala <sriharsha.k@caratlane.com>
2018-03-31 14:32:15 +0530

    Show imported repos in different color

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,9 @@ class SearchController < ApplicationController
   def index
     if params[:query].present?
       @repos = GithubService.search_repositories params[:query]
+      @repos_imported = Repository.all.map(&:name)
+      @current_repo_names = @repos['items'].map {|x| x['full_name']}
+      @repos_matching = @repos_imported & @current_repo_names
     end
   end
 
